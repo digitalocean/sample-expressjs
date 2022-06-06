d0182d4e3de1e1c298c4240ab79baae90317fdd3
gitmolejon <gitmolejon@gmail.com>
2022-06-06 23:47:09 +0100

    First commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const startupMessage = `
 [0m
 `
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Sasbobo...'))
 
 for (let line of startupMessage.split("\n")) {
 	console.log(line)
